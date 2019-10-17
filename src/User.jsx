a4ea04ede0156156298533a0458b949b3cf448f1
fabriceloc <fabrice.locqueville@viseo.com>
2019-10-17 16:10:23 +0200

    clean

--- src/User.jsx
+++ src/User.jsx
@@ -36,25 +36,6 @@ export default class User extends Component{
             })
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.match.params.idUser !== prevProps.match.params.idUser) {
-            axios.get(`${API_URL}` + this.props.match.params.idUser)
-                .then(({ data }) => {
-                    this.setState({
-                        result: data // MusicGraph returns an object named data,
-                        // as does axios. So... data.data
-                    })
-                    axios.get(`${API_URL_REPO}` + this.state.result.login + "/repos")
-                        .then(({ data }) => {
-                            this.setState({
-                                list: data // MusicGraph returns an object named data,
-                                // as does axios. So... data.data
-                            })
-                        })
-                })
-        }
-    }
-
     render() {
         let val = this.state.result;
         let list = this.state.list;
